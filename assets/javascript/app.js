48086d0c9bf2d2c46cc5596b88de1a827d22ded4
Will Vongkoth <avongkoth92@u.northwestern.edu>
2017-10-18 18:43:56 -0500

    got the api key to work and the buttons to generate dynamically as well as the gifs to display properly in the gif display div

---
+++ assets/javascript/app.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+//initial array of cartoons
+var topics = ["south park", "family guy", "the simpsons", "spongebob squarepants", "hey arnold", "rugrats"];
+
+
+
+$("#addCartoon").click(function(event){
+	event.preventDefault();
+	var cartoon = $("#cartoon-input").val().trim();
+	topics.push(cartoon);
+	gifCreator(cartoon);
+	renderButtons();
+});
+
+
+renderButtons();
+function gifCreator(cartoonGif){
+	$("#cartoonsGif").empty();
+	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=U62sFuIW53IcX3v7NrHAWTKp1gLSZPfx&q=" + cartoonGif + "&limit=25&offset=0&rating=G&lang=en";
+	console.log("hello" + cartoonGif);
+	$.ajax({
+		url : queryURL,
+		method : "GET"
+	}).done(function(response){
+		console.log(response.data.length);
+		for(var i =0; i < 4; i++){
+		var b = $("<img>");
+		b.addClass("gifResult");
+		b.attr("src", response.data[i].images.downsized.url);
+		$("#cartoonsGif").append(b);
+	}
+
+	})
+};
+
+function renderButtons(){
+
+	$("#cartoonButtons").empty();
+	for (var i = 0; i < topics.length; i++) {
+		var a = $("<button>");
+			a.addClass("cartoons");
+			a.attr("data-cartoon", topics[i]);
+			a.text(topics[i]);
+			a.click(function(){
+				gifCreator($(this).attr("data-cartoon"));
+			})
+			$("#cartoonButtons").append(a);
+		}
+};
+
+});
